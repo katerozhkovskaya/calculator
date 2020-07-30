fa3a85fb73387fe6676813b487e73e3af460c03b
katerozhkovskaya <katerozhkovskaya@gmail.com>
2020-07-30 18:28:42 +0000

    calkc

--- js/index.js
+++ js/index.js
@@ -1,66 +1,62 @@
-const display = document.querySelector('.display');
-const results = document.querySelector('.results');
+const display = document.querySelector(".display");
+const results = document.querySelector(".results");
 
-document.querySelectorAll('.digit')
-.forEach(button=>button.addEventListener('click', digitPressed));
+document
+  .querySelectorAll(".digit")
+  .forEach((button) => button.addEventListener("click", digitPressed));
 
-
-function digitPressed (ev){
-    const digit = ev.target.innerText;
-    display.value+=digit;
+function digitPressed(ev) {
+  const digit = ev.target.innerText;
+  display.value += digit;
 }
 
 //document.querySelectorAll('.koma')
 //.forEach(button=>button.addEventListener('click', digitPressed));
 
 let operation = "";
-let first_operand=0;
-let second_operand=0;
-document.querySelectorAll('.operation')
-.forEach(button=>button.addEventListener('click', operationPressed));
+let first_operand = 0;
+let second_operand = 0;
+document
+  .querySelectorAll(".operation")
+  .forEach((button) => button.addEventListener("click", operationPressed));
 
-function operationPressed (ev){
-    const oper = ev.target.value;
-    //if (operation ===""){
-
-    //}else{
-
-    //}
-
-    operation=oper;
-    first_operand=display.value;
-    display.value="";
-    
+function operationPressed(ev) {
+  const oper = ev.target.value;
+  operation = oper;
+  first_operand = display.value;
+  display.value = "";
 }
 
-document.querySelectorAll('.equal')
-.forEach(button=>button.addEventListener('click', equalPressed));
+document
+  .querySelectorAll(".equal")
+  .forEach((button) => button.addEventListener("click", equalPressed));
 
-function equalPressed (ev){
-    second_operand=display.value;
-    let result=0;
-    switch(operation){
+function equalPressed(ev) {
+  second_operand = display.value;
+  let result = 0;
+  switch (operation) {
     case "+":
-    result=parseFloat(first_operand)+parseFloat(second_operand);
-    break;
+      result = parseFloat(first_operand) + parseFloat(second_operand);
+      break;
     case "-":
-    result=parseFloat(first_operand)-parseFloat(second_operand);
-    break;
+      result = parseFloat(first_operand) - parseFloat(second_operand);
+      break;
     case "/":
-    result=parseFloat(first_operand)/parseFloat(second_operand);
-    break;  
+      result = parseFloat(first_operand) / parseFloat(second_operand);
+      break;
     case "*":
-    result=parseFloat(first_operand)*parseFloat(second_operand);
-    break; 
-          
-    }
-    results.innerHTML+=first_operand+operation+second_operand+"="+result+"<br/>";
-    display.value=result;
+      result = parseFloat(first_operand) * parseFloat(second_operand);
+      break;
+  }
+  results.innerHTML +=
+    first_operand + operation + second_operand + "=" + result + "<br/>";
+  display.value = result;
 }
 
-document.querySelectorAll('.clear')
-.forEach(button=>button.addEventListener('click', clearPressed));
+document
+  .querySelectorAll(".clear")
+  .forEach((button) => button.addEventListener("click", clearPressed));
 
-function clearPressed (){
-    display.value="";
+function clearPressed() {
+  display.value = "";
 }
